4ef58c11d36a4476d860cc4550d8600d515645fc
Adam Robinson <ajrobin@me.com>
2018-11-26 17:21:40 -0600

    Top layout completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="menu">
+          {/* logo created at https://fontmeme.com/netflix-font/ */}
+          <img className="logo" src="https://fontmeme.com/permalink/181125/c9709e8b1ab7623513cd9eaa0e49380a.png" alt="Adam Robinson" border="0" />
+        </div>
+        <div id="top">
+          <video autoPlay loop id="video-background" muted>
+            <source src="./media/typing2.mp4" type="video/mp4" />
+          </video>
+          <div id="topInfo">
+            {/* logo created at https://fontmeme.com/netflix-font/ */}
+            <img className="logo" src="https://fontmeme.com/permalink/181125/c9709e8b1ab7623513cd9eaa0e49380a.png" alt="Adam Robinson" border="0"/>
+            <h3>913.787.6177</h3>
+            <h3>ajrobin@me.com</h3>
+          </div>
+        </div>
+
+        <div id="body">
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+        </div>
       </div>
     );
   }
