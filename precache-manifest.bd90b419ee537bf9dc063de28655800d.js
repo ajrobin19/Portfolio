400a2c309e0b6242be6ca14aaf85a2d6f79f9f4e
Adam Robinson <ajrobin@me.com>
2019-03-13 17:21:09 -0500

    Updates

---
+++ precache-manifest.bd90b419ee537bf9dc063de28655800d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "d7c155b851174f751cd6",
+    "url": "/portfolio/static/js/main.8cafe47b.chunk.js"
+  },
+  {
+    "revision": "6f0c8ac9d886ea0650cb",
+    "url": "/portfolio/static/js/2.f205da87.chunk.js"
+  },
+  {
+    "revision": "d7c155b851174f751cd6",
+    "url": "/portfolio/static/css/main.a558fb1d.chunk.css"
+  },
+  {
+    "revision": "f703b734695656793feef108ed3fa75c",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.9e5595eb58f42b017f15ffc3a4652b3d.js"
+  "/portfolio/precache-manifest.bd90b419ee537bf9dc063de28655800d.js"
 );
 
 workbox.clientsClaim();
