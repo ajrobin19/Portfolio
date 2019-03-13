f1531b79bf5d79dc7791dfda3cee4d6d95bacaa8
Adam Robinson <ajrobin@me.com>
2019-03-13 17:19:30 -0500

    Made a few small tweeks. Also added coding skills section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import jsonData from '../src/data/information.json';
+import skills from '../src/data/skills.json';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearchPlus as enlarge, faStar as wholeStar} from '@fortawesome/free-solid-svg-icons'
+import { faStar as hollowStar } from '@fortawesome/free-regular-svg-icons'
+import { faGithubSquare as github, faLinkedin as linkedin, faFacebookSquare as facebook } from '@fortawesome/free-brands-svg-icons'
+
+library.add(enlarge, wholeStar, hollowStar, github, linkedin, facebook)
 
 class App extends Component {
 
@@ -109,6 +118,11 @@ class App extends Component {
             <img className="logo" src="./media/whiteLogo.png" alt="Adam Robinson" border="0"/>
             <h3>913.787.6177</h3>
             <h3>ajrobin@me.com</h3>
+            <div className="icons">
+              <a className="fai" href="https://www.linkedin.com/in/adam-robinson-090b1914a/" target="_blank"><FontAwesomeIcon icon={['fab', 'linkedin']} size='2x' /></a>
+              <a className="fai" href="https://github.com/ajrobin19/" target="_blank"><FontAwesomeIcon icon={['fab', 'github-square']} size='2x' /></a>
+              <a className="fai" href="https://www.facebook.com/adam.robinson/" target="_blank"><FontAwesomeIcon icon={['fab', 'facebook-square']} size='2x' /></a>
+            </div>
           </div>
         </div>
 
@@ -136,9 +150,9 @@ class App extends Component {
                 {this.infoObject[results.reference] &&
                 <div id={results.reference +"info"} className="info" >
                   <div className="exit" onClick={e => {this.makeFalse(results.reference); this.forceUpdate()}}>X</div>
-                  {results.url && results.image && <a href={results.url} target="_blank"><img className="degree" src={results.image} alt={results.name +" Degree"} /></a>}
-                  {!results.url && results.image && <img className="degree" src={results.image} alt={results.name +" Degree"} />}
-                  {results.image2 && <img className="degree2" src={results.image2} alt={results.name +" Degree"} />}
+                  {results.url && results.image && <a className="degree" href={results.url} target="_blank"><img  src={results.image} alt={results.name +" Degree"} /><span className='enlarge'><FontAwesomeIcon icon={['fas', 'search-plus']} /></span></a>}
+                  {!results.url && results.image && <span className="degree"><img  src={results.image} alt={results.name +" Degree"} /><span className='enlarge'><FontAwesomeIcon icon={['fas', 'search-plus']} /></span></span>}
+                  {results.image2 && <span className="degree2"><img src={results.image2} alt={results.name +" Degree"} /><span className='enlarge'><FontAwesomeIcon icon={['fas', 'search-plus']} /></span></span>}
                   {results.name && <h1 style={{marginTop: 0}}>{results.name}</h1>}
                   {results.url && <h3><a href={results.url} target="_blank">{results.url}</a></h3>}
                   {results.duration && <h3>{results.duration}</h3>}
@@ -158,6 +172,51 @@ class App extends Component {
               )}
             </div>
           )}
+          {skills !== undefined && 
+            <div id={skills.name}>
+              <h1>{skills.name}</h1>
+              <div id={skills.reference+"section"} className="section" onMouseEnter={e => this.showArrows(skills.reference)} onMouseLeave={e => this.hideArrows(skills.reference)}>
+                <div id={skills.reference} className="row">
+                  <div id={skills.reference+"Left"} className="scrollButton left" onMouseEnter={e => this.scroll('left', skills.reference)} onMouseLeave={e => this.cancelScroll()}>
+                    <img src="./media/Arrow-Left-icon.png" alt="Left Scroll Button"/>
+                  </div>
+                  <div id={skills.reference+"Right"} className="scrollButton right" onMouseEnter={e => this.scroll('right', skills.reference)} onMouseLeave={e => this.cancelScroll()}>
+                    <img src="./media/Arrow-Right-icon.png" alt="Left Scroll Button"/>
+                  </div>
+                  {skills.results !== undefined && skills.results.map((results) =>
+                    <div id={results.reference} className="column" onClick={e => {this.makeTrue(results.reference); document.getElementById(results.reference).scrollIntoView(); this.forceUpdate()}} style={{backgroundColor: results.backgroundColor, backgroundImage: 'url('+results.backgroundImage+')', backgroundSize: results.backgroundSize, backgroundPosition: results.backgroundPosition, backgroundRepeat: 'no-repeat'}}>
+                      <p>{results.name}</p>
+                    </div>
+                  )}
+                </div>
+              </div>
+              {skills.results.map((results) =>
+                <div>
+                {this.infoObject[results.reference] &&
+                <div id={results.reference +"info"} className="info" >
+                  <div className="exit" onClick={e => {this.makeFalse(results.reference); this.forceUpdate()}}>X</div>
+                  {results.url && results.image && <a className="degree" href={results.url} target="_blank"><img  src={results.image} alt={results.name +" Degree"} /><span className='enlarge'><FontAwesomeIcon icon='search-plus' /></span></a>}
+                  {!results.url && results.image && <span className="degree"><img  src={results.image} alt={results.name +" Degree"} /><span className='enlarge'><FontAwesomeIcon icon='search-plus' /></span></span>}
+                  {results.image2 && <span className="degree2"><img src={results.image2} alt={results.name +" Degree"} /><span className='enlarge'><FontAwesomeIcon icon='search-plus' /></span></span>}
+                  {results.name && <h1 style={{marginTop: 0}}>{results.name}</h1>}
+                  {results.url && <h3><a href={results.url} target="_blank">{results.url}</a></h3>}
+                  {results.duration && <h3>{results.duration}</h3>}
+                  <br />
+                  <br />
+                  {results.degrees && results.degrees.map((deg)=><h4>{deg}</h4>)}
+                  {results.position && <h3>{results.position}</h3>}
+                  {results.position && results.position.endsWith("Teacher") && <h4>As a teacher at this school...</h4>}
+                  {results.position && !results.position.endsWith("Teacher") && <h4>As an employee at this company...</h4>}
+                  {results.points && results.points.map((point)=><h4><li>{point}</li></h4>)}
+                  <br />
+                  <br />
+                  {results.comments && <p>{results.comments}</p>}
+                </div>
+              }
+              </div>
+              )}
+            </div>
+          }
         </div>
       </div>
     );
